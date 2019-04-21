63e8d6afa444384775f564c5a4b47060af8428d5
Andy <Andy@gmail.com>
2019-04-21 10:37:21 +0800

    update

--- app/src/main/java/com/xzl/demo1/IndexActivity.java
+++ app/src/main/java/com/xzl/demo1/IndexActivity.java
@@ -11,7 +11,7 @@ import android.support.v7.app.AppCompatActivity;
  * 如果是SurfaceView重叠，那么有问题
  * TextureView重叠,可以
  * <p>
- * SurfaceView:尝试使用Visibile模式,其他模式在探索 https://blog.csdn.net/email_jade/article/details/82895335
+ * SurfaceView:尝试使用Visibile模式,但是visible会引发surfaceview的重建和销毁,其他模式在探索 https://blog.csdn.net/email_jade/article/details/82895335
  * https://www.jianshu.com/p/d558a4c9c868
  * <p>
  * https://blog.csdn.net/gfg156196/article/details/72899287
@@ -23,6 +23,8 @@ import android.support.v7.app.AppCompatActivity;
  *
  * https://www.jianshu.com/p/d558a4c9c868
  *
+ *
+ *
  * getChildDrawIndex
  *
  * SurfaceView setVisible会重建
@@ -30,7 +32,12 @@ import android.support.v7.app.AppCompatActivity;
  *  设置top也没法切换，只能设置一次
  * http://www.dewen.net.cn/q/17182/%E5%A4%9A%E4%B8%AASurfaceView%E9%87%8D%E5%8F%A0%E3%80%81%E5%B1%82%E7%BA%A7%E6%98%BE%E7%A4%BA%2C%E5%A6%82%E4%BD%95%E5%B8%83%E5%B1%80
  *
+ * https://blog.csdn.net/u011803341/article/details/78717635
  *
+ * https://blog.csdn.net/wenxiang423/article/details/82662113 原生循环播放视频不黑屏
+ * https://blog.csdn.net/fkgjdkblxckvbxbgb/article/details/82812821
+ *
+ * Surfaceview原理 ：https://blog.csdn.net/Luoshengyang/article/details/8661317
  *
  */
 
