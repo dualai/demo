9a475cba6fedddae8d057ddb0260672426b47551
xuluming <xuluming@zailingtech.com>
2019-04-28 18:24:36 +0800

    'update'

--- app/src/main/java/com/xzl/demo1/life/LifeCyclerActivity.java
+++ app/src/main/java/com/xzl/demo1/life/LifeCyclerActivity.java
@@ -7,11 +7,20 @@ import android.support.v7.app.AppCompatActivity;
 import com.xzl.demo1.R;
 
 public class LifeCyclerActivity extends AppCompatActivity {
+    private MapLocation mapLocation;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lifecycle);
 
+        mapLocation = new MapLocation(getLifecycle());
+        getLifecycle().addObserver(mapLocation);
+    }
 
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        getLifecycle().removeObserver(mapLocation);
     }
 }
